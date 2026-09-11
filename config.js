f98ca9be3fea38926941ca12e63e95ecbb03b11d
aanbramak-cyber <aanbrama.k@gmail.com>
2026-09-11 10:36:11 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Alamat API (URL aplikasi web Apps Script yang berakhiran /exec).
+window.SOMTUM_API_URL = "https://script.google.com/macros/s/AKfycbz9h1FBA0GpceyPijC0E9WYEjeliLHNtsRzDHL7dMA0SAUmwYdtCNgV9Ia8SjV10l9u/exec";
+window.SOMTUM_VERSION = "1.0.0";
