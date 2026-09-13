091d7ce7e257264e56cff09e03578a3e1fa44cd2
aanbramak-cyber <aanbrama.k@gmail.com>
2026-09-13 17:17:21 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 // Alamat API (URL aplikasi web Apps Script yang berakhiran /exec).
 window.SOMTUM_API_URL = "https://script.google.com/macros/s/AKfycbz9h1FBA0GpceyPijC0E9WYEjeliLHNtsRzDHL7dMA0SAUmwYdtCNgV9Ia8SjV10l9u/exec";
-window.SOMTUM_VERSION = "1.0.0";
+// Alamat "Somtum Inventory API" (web app read-only untuk menu Cek Stock & Laporan Stok).
+// Isi URL /exec-nya setelah deploy. Kalau kosong, menu Cek Stock akan bilang "backend belum disetel".
+window.INVENTORY_API = "https://script.google.com/macros/s/AKfycbzhnEvLW3VRIczQVaXy50GSD0crpujeEfA9WgD6g7bc-GPlZPtDvvCeY3M6AF4zXYgs/exec";
+window.SOMTUM_VERSION = "1.2.0";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'somtum-app-v1';
+const CACHE = 'somtum-app-v3';
 const SHELL = ['./', './index.html', './config.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
